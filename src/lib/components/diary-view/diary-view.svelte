f15baf95bfc5061a701d4404a0fe10b39562b225
isaxk <76838845+isaxk@users.noreply.github.com>
2025-11-08 18:03:28 +0000

    test git commit

--- src/lib/components/diary-view/diary-view.svelte
+++ src/lib/components/diary-view/diary-view.svelte
@@ -66,7 +66,7 @@
 									<div class="flex w-5 justify-center">
 										<Check size={20} />
 									</div>
-									Saved
+									Saved. You can edit for the rest of the day.
 								{/if}
 							</div>
 						</div>
